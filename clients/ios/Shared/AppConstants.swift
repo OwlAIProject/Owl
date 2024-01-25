9f7c9d31db88269343e7cd7f583f74aae1b076eb
Ethan Sutin <ethan@sutin.net>
2024-01-25 05:38:44 -0800

    Remove local base url

--- clients/ios/Shared/AppConstants.swift
+++ clients/ios/Shared/AppConstants.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct AppConstants {
-    static let apiBaseURL = "http://47.33.18.169:8000"
+    static let apiBaseURL = ""
     static let bleServiceUUID = "03d5d5c4-a86c-11ee-9d89-8f2089a49e7e"
     static let bleAudioCharacteristicUUID = "b189a505-a86c-11ee-a5fb-8f2089a49e7e"
 }
