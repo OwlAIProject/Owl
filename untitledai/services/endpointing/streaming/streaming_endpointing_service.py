15b67d9ad6463faec51c627627f071f02579fec7
Ethan Sutin <ethan@sutin.net>
2024-01-27 10:33:03 -0800

    Fix stub endpointer

--- untitledai/services/endpointing/streaming/streaming_endpointing_service.py
+++ untitledai/services/endpointing/streaming/streaming_endpointing_service.py
@@ -7,25 +7,25 @@ class StreamingEndpointingService(AbstractStreamingEndpointingService):
         self.min_utterances = min_utterances
         self.endpoint_callback = endpoint_callback
         self._utterance_count = 0
-        self._first_utterance_time = None
+        self._last_utterance_time = None
         self._endpoint_called = False
         self._timeout_task = asyncio.create_task(self._check_timeout())
 
     async def utterance_detected(self):
-        if self._first_utterance_time is None:
-            self._first_utterance_time = asyncio.get_event_loop().time()
+        current_time = asyncio.get_event_loop().time()
+        self._last_utterance_time = current_time
         self._utterance_count += 1
 
     async def _check_timeout(self):
         while True:
             await asyncio.sleep(1)
-            if self._first_utterance_time is None:
+            if self._last_utterance_time is None:
                 continue
 
             current_time = asyncio.get_event_loop().time()
-            time_elapsed_since_first = current_time - self._first_utterance_time
+            time_elapsed_since_last = current_time - self._last_utterance_time
 
-            if (time_elapsed_since_first >= self.timeout_interval and
+            if (time_elapsed_since_last >= self.timeout_interval and
                 self._utterance_count >= self.min_utterances and
                 not self._endpoint_called):
                 if self.endpoint_callback:
@@ -34,7 +34,7 @@ class StreamingEndpointingService(AbstractStreamingEndpointingService):
 
     def _reset(self):
         self._utterance_count = 0
-        self._first_utterance_time = None
+        self._last_utterance_time = None
         self._endpoint_called = False
         self._timeout_task.cancel()
         self._timeout_task = asyncio.create_task(self._check_timeout())
