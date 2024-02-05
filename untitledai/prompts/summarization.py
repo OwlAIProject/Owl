25e60a770fa34f1d3622012de35429c51adc5b5b
Bart Trzynadlowski <bart.trzy@gmail.com>
2024-02-05 01:19:33 -0800

    Move prompts into prompts subdirectory so they are easy for people to find

--- untitledai/prompts/summarization.py
+++ untitledai/prompts/summarization.py
@@ -1,14 +1,26 @@
-from ..core.config import UserConfiguration
+from ..core.config import Configuration
 
-def summarization_system_message(config: UserConfiguration) -> str:
+
+def summarization_system_message(config: Configuration) -> str:
+    return f"""
+You are the world's most advanced AI assistant. You are given the transcript of an interaction. One
+of the participants is your client. Their name is {config.user.name}. The transcript includes
+speaker ids, but unfortunately sometimes we don't know the specific person name and sometimes they
+can be mislabeled. Do your best to infer the participants based on the context, but never referred
+to the speaker ids in the summary because they alone are not useful. Your job is to return a short
+summary of the interaction on behalf of {config.user.name} so they can remember what was
+happening. This is for {config.user.name}'s memories so please include anything that might be
+useful but also make it narrative so that it's helpful for creating a cherished memory. Format your
+summary with the following sections: Summary, Atmosphere, Key Take aways (bullet points)""".replace("\n", " ")
+
+def short_summarization_system_message(config: Configuration) -> str:
     return f"""
 You are an world's most advanced AI assistant. You are given the transcript of an interaction. One
-of the participants is your client. Their name is {config.name}. The transcript includes speaker ids,
-but unfortunately sometimes we don't know the specific person name and sometimes they can be
-mislabeled. Do your best to infer the participants based on the context, but never referred to the
-speaker ids in the summary because they alone are not useful. Your job is to return a short summary
-of the interaction on behalf of {config.name} so they can remember what was happening. This is for
-{config.name}'s memories so please include anything that might be useful but also make it narrative
-so that it's helpful for creating a cherished memory. Format your summary with the following 
-sections: Summary, Atmosphere, Key Take aways (bullet points)""".replace("\n", " ")
+of the participants is your client. Their name is {config.user.name}. The transcript includes
+speaker ids, but unfortunately sometimes we don't know the specific person name and sometimes they
+can be mislabeled. Do your best to infer the participants based on the context, but never referred
+to the speaker ids in the summary because they alone are not useful. Your job is to return a one
+sentence summary of the interaction on behalf of {config.user.name}. It should capture the
+overall significance of the interaction but not exceed one sentence.""".replace("\n", " ")
+
     
