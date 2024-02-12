546816d98f83e64accd0f80a8ba5b8a6526a639b
Bart Trzynadlowski <bart.trzy@gmail.com>
2024-02-11 21:40:38 -0800

    Clarifying comment

--- untitledai/models/datetime_serialization.py
+++ untitledai/models/datetime_serialization.py
@@ -2,6 +2,12 @@
 # datetime_serialization.py
 #
 # Serialization of datetimes into strings in a standardized way for the server and display clients.
+# This is only intended for serializing database objects and not e.g., the capture directory, which
+# uses a different format.
+#
+# TODO:
+# -----
+# - We should probably standardize on %Y%m%d-%H%M%S.%f, like the capture files, instead.
 #
 
 from datetime import datetime
