064b5a6358c766f5cebd039d9f4c7741e5d0c2a2
Ethan Sutin <ethan@sutin.net>
2024-02-16 08:54:42 -0800

    More logging

--- untitledai/services/stt/asynchronous/async_whisper_transcription_service.py
+++ untitledai/services/stt/asynchronous/async_whisper_transcription_service.py
@@ -25,9 +25,10 @@ class AsyncWhisperTranscriptionService(AbstractAsyncTranscriptionService):
             response = await self.http_client.post(url, json=payload)
             response.raise_for_status()
             response_string = response.text 
-
+            logger.info(f"Received response from local async whisper server: {response_string}")
             transcript_response = TranscriptionResponse.model_validate_json(response_string)
             utterances = []
+            logger.info(f"Transcription response: {transcript_response}")
             for whisper_utterance in transcript_response.utterances:
                 utterance = Utterance(
                     start=whisper_utterance.start,
@@ -56,3 +57,5 @@ class AsyncWhisperTranscriptionService(AbstractAsyncTranscriptionService):
             logger.error(f"Error response {e.response.status_code} while requesting {e.request.url!r}.")
         except httpx.RequestError as e:
             logger.error(f"An error occurred while requesting {e.request.url!r}.")
+        except Exception as e:
+            logger.error(f"An unexpected error occurred while requesting {url}: {e}")
